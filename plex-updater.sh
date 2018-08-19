4b7ccd7c141904a5f850e0cafc407df0bf0e8dde
Strux <astrujic@gmail.com>
2018-08-20 01:43:51 +0200

    fixing

--- plex-updater.sh
+++ plex-updater.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Author: Adnan Strujic
 # Email: astrujic@gmail.com
-# Date: March 2016
+# Date: August 2018
 # License: Apache License 2.0 - http://www.apache.org/licenses/
 
 INSTALLED_VER="$(apt-cache show plexmediaserver | awk '/Version/ {print $2}')"
-LATEST_VER="$(curl -s https://plex.tv/downloads | grep -Po '(?<=plexmediaserver_).*?(?=_amd64\.deb)')"
+LATEST_VER="$(curl -s https://plex.tv/api/downloads/1.json | grep -Po '(?<=plexmediaserver_).{10,30}(?=_amd64\.deb)')"
 INSTALL_FILE="plexmediaserver_"$LATEST_VER"_amd64.deb"
 INSTALL_FOLDER="/srv/scripts/"
 
@@ -16,6 +16,7 @@ if [ "$INSTALLED_VER" != "$LATEST_VER" ]
 then
 # Download new version
 wget -P $INSTALL_FOLDER 'https://downloads.plex.tv/plex-media-server/'$LATEST_VER'/'$INSTALL_FILE
+
 # Install new version
 dpkg -i $INSTALL_FOLDER$INSTALL_FILE
 # Remove installation file
